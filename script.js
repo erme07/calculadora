3dbbe03a4e8ceb9658d6c09baf561ff523e80937
erme07 <erik.medina07@gmail.com>
2022-09-13 14:34:07 -0300

    Actualizacion con mas funcionalidades, emulando una calculadora convencional

--- script.js
+++ script.js
@@ -1,107 +1,186 @@
 
 const numBoton = document.querySelectorAll('[name="numero"], [name="operador"]');
-const formula = document.getElementById("formula");
-const blink = document.getElementById("blink");
+const formula = document.querySelector(".formula");
+const blink = document.querySelector(".blink");
 const resultado = document.getElementById("resultado");
 const borrar = document.getElementById("eliminar");
 const reset = document.getElementById("reset");
 const operar = document.getElementById("operar");
-const simboloAns = document.getElementById("Ans");
-const ans = document.getElementById("agregar");
+const simboloAns = document.querySelector(".Ans");
+const exponente = document.querySelector(".exponente");
+const base = document.querySelector(".base");
+const flechaIzq = document.querySelector(".flechaIzq");
 
+let formulaBack='',formulaAux='',formulaAns='' , ansActive=false, igual=false, error=false;
 
-let formulaFront = formula.innerHTML, formulaBack='',formulaAux='',formulaAns='',valorAns='' , ansActive=false, igual=false;
+function verificarError(){
+    if(ansActive){
+        if(formula.innerHTML[0]==='+' || formula.innerHTML[0]==='×'||formula.innerHTML[0]==='÷' ||formula.innerHTML[0]==='-'){
+            return true;
+        }else if(formula.innerHTML===''){
+            return false;
+        }else{
+            simboloAns.innerHTML='';
+            blink.innerHTML='';
+            formula.innerHTML='&nbsp;Sintax Error';
+            resultado.innerHTML='';
+            error=true;
+            ansActive=false;
+            return false;
+        }
+    }else if(formula.innerHTML===''){
+        return false;
+    }else{
+        console.log("b");
+        return true;
+    }
+    /*Por ahora solo detecta algunos errores de sintaxys relacionados al uso de "Ans"
+    Falta desarrollo para detectar mas tipos de errores 
+    al igual que las calculadoras convencionales */
+}
 
 function ejecutar(){
-    formulaBack=formulaFront;
-    formulaBack=formulaBack.split('');
-    for(let i=0;i<formulaBack.length;i++){
-        if(formulaBack[i]==='×'){
-            formulaBack[i]='*';
+     if(verificarError() && !error){
+        if(ansActive){
+            formulaBack=formulaAns;
+        }else{
+            formulaBack=formula.innerHTML;
         }
-        else if(formulaBack[i]=='÷'){
-            formulaBack[i]='/';
+        formulaBack=formulaBack.split('');
+        for(let i=0;i<formulaBack.length;i++){
+            if(formulaBack[i]==='×'){
+                formulaBack[i]='*';
+            }
+            else if(formulaBack[i]=='÷'){
+                formulaBack[i]='/';
+            }
         }
-    }
-    formulaBack=formulaBack.join('');
-    resultado.innerHTML=eval(formulaBack);
-    console.log(eval(formulaBack).toFixed(1));
+        formulaBack=formulaBack.join('');
+        resultado.innerHTML=eval(formulaBack);
+        //notacionExpo();
+        igual=true;
+        blink.innerHTML='';
+     }
+}
 
-    igual=true;
-    blink.innerHTML='';
-}
 function eliminar(){
-    if(resultado.innerHTML==='0'){
-        formulaAux = formulaFront.slice(0,-1);
-        formulaFront = formulaAux;
-        formula.innerHTML=formulaFront;
+    if(!igual && !error){
+        if(ansActive && formula.innerHTML.length === 0){
+            ansActive=false;
+            simboloAns.innerHTML='';
+        }
+            formulaAux=formulaAns.slice(0,-1);
+            formulaAns=formulaAux;
+            formulaAux='';
+            formulaAux=formula.innerHTML.slice(0,-1);
+            formula.innerHTML=formulaAux;
+            formulaAux='';
+            desplazarTexto();
     }
 }
+
 function limpiarFormula(){
-    formulaFront='';
-    formula.innerHTML=formulaFront;
-}
-function resetear(){
-    formulaFront='';
+    formula.innerHTML='';
     blink.innerHTML='_';
-    formula.innerHTML=formulaFront;
+}
+
+function resetear(){
+    limpiarFormula();
     resultado.innerHTML='0';
     ansActive=false;
     simboloAns.innerHTML='';
+    exponente.classList.remove('mostrar');
+    base.classList.remove('mostrar');
+    flechaIzq.classList.remove('mostrar');
+    error=false;
 }
 
 
 function modoAns(i){
-    if(numBoton[i].getAttribute("name") ==='operador' && resultado.innerHTML!='0' && igual){
+    if((numBoton[i].getAttribute("name") ==='operador' && resultado.innerHTML!='0' && igual) || numBoton[i].innerHTML==='ANS' && !error){
         ansActive=true;
+        igual=false;
         simboloAns.innerHTML="Ans";
-        valorAns=resultado.innerHTML;
-        formulaFront=valorAns;
-        formula.innerHTML='';
+        formulaAns=resultado.innerHTML;
+        limpiarFormula();
+    }else if(numBoton[i].getAttribute("name")==='numero' && ansActive && igual){
+        ansActive=false;
+        simboloAns.innerHTML='';
+        formulaAns='';
     }
-    if(numBoton.innerHTML==='ANS'){
-        ansActive=true;
-        simboloAns.innerHTML="Ans";
-        valorAns=resultado.innerHTML;
-        formulaFront=valorAns;
-        formula.innerHTML='';
+    if(ansActive){
+        if(numBoton[i].innerHTML === '·' || numBoton[i].innerHTML === '−'){
+            formulaAns+=numBoton[i].getAttribute("data-valor");
+            formula.innerHTML+=numBoton[i].getAttribute("data-valor");
+        }else if(numBoton[i].innerHTML != 'ANS'){
+            formulaAns+=numBoton[i].innerHTML;
+            formula.innerHTML+=numBoton[i].innerHTML;
+        }
     }
+    
+}
+
+function desplazarTexto(){
+    if(ansActive){
+        if(formula.innerHTML.length>10){
+            formula.classList.add('desplazar');
+            flechaIzq.classList.add('mostrar');
+            simboloAns.innerHTML='';
+        }else{
+            formula.classList.remove('desplazar');
+            flechaIzq.classList.remove('mostrar');
+            simboloAns.innerHTML='Ans';
+        }
+    }else if(!error){
+        if(formula.innerHTML.length>13){
+            formula.classList.add('desplazar');
+            flechaIzq.classList.add('mostrar');
+        }else{
+            formula.classList.remove('desplazar');
+            flechaIzq.classList.remove('mostrar');
+        }
+    }
+    
+}
+
+
+
+function notacionExpo(){
+    if(resultado.innerHTML.length>12){
+        exponente.classList.add("mostrar");
+        base.classList.add("mostrar");
+        /*falta implementar codigo para representar
+         en notacion cientifica o exponencial los numeros 
+         grandes que no caben en pantalla*/
+
+    }else{
+        exponente.classList.remove("mostrar");
+        base.classList.remove("mostrar");
+    }
+    
 }
 
 numBoton.forEach((e,i) => {
     e.onclick=function(){
-        blink.innerHTML='_';
         modoAns(i);
-        if(ansActive){
-            
-            if(e.innerHTML === '·' || e.innerHTML === '−'){
-                formulaFront+=e.getAttribute("data-valor");
-                formula.innerHTML+=e.getAttribute("data-valor");
-            }else if(e.innerHTML != 'ANS'){
-                formulaFront+=e.innerHTML;
-                formula.innerHTML+=e.innerHTML;
-            }
-            
-        }else if(resultado.innerHTML!='0' && igual){
-            limpiarFormula();
-            igual=false;
-            if(e.innerHTML === '·' || e.innerHTML === '−'){
-                formulaFront+=e.getAttribute("data-valor");
-                formula.innerHTML=formulaFront;
-            }else{
-                formulaFront+=e.innerHTML;
-                formula.innerHTML=formulaFront;
-            }
-        }else {
-            if(e.innerHTML === '·' || e.innerHTML === '−'){
-                formulaFront+=e.getAttribute("data-valor");
-                formula.innerHTML=formulaFront;
-            }else{
-                formulaFront+=e.innerHTML;
-                formula.innerHTML=formulaFront;
+        if(!ansActive){
+            if(resultado.innerHTML!='0' && igual){
+                limpiarFormula();
+                igual=false;
+                if(e.innerHTML === '·' || e.innerHTML === '−'){
+                    formula.innerHTML+=e.getAttribute("data-valor");
+                }else{
+                    formula.innerHTML+=e.innerHTML;
+                }
+            }else if(!error){
+                if(e.innerHTML === '·' || e.innerHTML === '−'){
+                    formula.innerHTML+=e.getAttribute("data-valor");
+                }else{
+                    formula.innerHTML+=e.innerHTML;
+                }
             }
         }
-        
+        desplazarTexto();
     };
 });
 
